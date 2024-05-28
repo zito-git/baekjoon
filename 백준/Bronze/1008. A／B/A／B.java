c7d2fe10ec67b4418b4c4bc07130b530ecdee9b4
지토지토 <strongpower.zito@gmail.com>
2024-05-28 16:31:30 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double n1 = scanner.nextDouble();
+
+        Double n2 = scanner.nextDouble();
+
+        System.out.println(n1 / n2);
+    }
+}
